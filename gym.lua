a5d6c0ab875d931bbdb86443c1b18eca63eb8b73
Squid Coder <squid@squidcoder.com>
2025-03-02 19:01:38 -0600

    Update the documentation

--- gym.lua
+++ gym.lua
@@ -5,20 +5,20 @@ Gym
 
 Tags: Fort| Needs | BugFix | Units
 
-Assigns Dwarves to a military squad until they have fulfilled their need for Martial Training. Also Passively builds military skills and physical stats.
+Assigns Dwarves to a military squad until they have fulfilled their need for Martial Training. Also passively builds military skills and physical stats.
 
 CRITICAL SETUP:
-01-Minimum 1 squad with the name "Gym"
-02-An assigned squadleader in "Gym"
-03-An assigned Barracks for the squad "Gym"
-04-Active Training orders for the squad "Gym"
 
-This should be a new non military use squad. The uniform should be set to "No Uniform" and the squad should be set to "Constant Training" in the military screen.
+- Minimum 1 squad with the name "Gym"
+- An assigned squadleader in "Gym"
+- An assigned Barracks for the squad "Gym"
+- Active Training orders for the squad "Gym"
+
+This should be a new non-military-use squad. The uniform should be set to "No Uniform" and the squad should be set to "Constant Training" in the military screen.
 Set the squad's schedule to full time training with at least 8 or 9 training.
 The squad doesn't need months off. The members leave the squad once they have gotten their gains.
 
-NOTE-Dwarfs with the labor "Fish Dissection" enabled are ignored
-Make a Dwarven labour with only the Fish Dissection enabled, set to "Only selected do this" and assign it to a dwarf to ignore them.
+NOTE: Dwarfs with the labor "Fish Dissection" enabled are ignored. Make a Dwarven labour with only the Fish Dissection enabled, set to "Only selected do this" and assign it to a dwarf to ignore them.
 
 Usage
 -----
@@ -32,18 +32,18 @@ Gym
     Current status of script
 
 Gym -start
-    checks to see if you have fullfilled the creation of a training gym
-    searches your fort for dwarves with a need to go to the gym, and begins assigning them to said gym.
+    Checks to see if you have fullfilled the creation of a training gym.
+    Searches your fort for dwarves with a need to go to the gym, and begins assigning them to said gym.
     Once they have fulfilled their need they will be removed from the gym squad to be replaced by the next dwarf in the list.
 
 Gym -stop
-    Dwarves currently in the squad ,with the exception of the squadleader, will be unassigned and no new dwarves will be added to the squad.
+    Dwarves currently in the Gym squad, with the exception of the squad leader, will be unassigned and no new dwarves will be added to the squad.
 
 Options
 -------
     -start
         Starts the script
-        If there is no squad named GYM with a squadleader assigned it will not proceed.
+        If there is no squad named `Gym` with a squadleader assigned it will not proceed.
 
     -stop
         Stops the script
