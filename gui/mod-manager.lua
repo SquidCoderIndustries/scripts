89c7d1396f9376e076c43b846aaa6f0e7aa237c1
Squid Coder <squid@squidcoder.com>
2025-04-12 17:51:08 -0500

    Update mod-manager.lua

--- gui/mod-manager.lua
+++ gui/mod-manager.lua
@@ -8,6 +8,8 @@ local dialogs = require('gui.dialogs')
 local json = require('json')
 local utils = require('utils')
 
+local scriptmanager = require('script-manager')
+
 local presets_file = json.open("dfhack-config/mod-manager.json")
 local GLOBAL_KEY = 'mod-manager'
 local INSTALLED_MODS_PATH = 'data/installed_mods/'
@@ -407,39 +409,10 @@ ModlistMenu.ATTRS {
     frame_title = "Active Modlist",
 
     resize_min = { w = 30, h = 15 },
-    frame = { w = 40, t = 10, b = 15 },
+    frame = { w = 40, h = 15 },
 
     resizable = true,
 }
-
-local function get_mod_id_and_version(path)
-    local idfile = path .. '/info.txt'
-    local ok, lines = pcall(io.lines, idfile)
-    if not ok then return end
-    local id, version
-    for line in lines do
-        if not id then
-            _,_,id = line:find('^%[ID:([^%]]+)%]')
-        end
-        if not version then
-            -- note this doesn't include the closing brace since some people put
-            -- non-number characters in here, and DF only reads the leading digits
-            -- as the numeric version
-            _,_,version = line:find('^%[NUMERIC_VERSION:(%d+)')
-        end
-        -- note that we do *not* want to break out of this loop early since
-        -- lines has to hit EOF to close the file
-    end
-    return id, version
-end
-
-local function add_mod_paths(mod_paths, id, base_path, subdir)
-    local sep = base_path:endswith('/') and '' or '/'
-    local path = ('%s%s%s'):format(base_path, sep, subdir)
-    if dfhack.filesystem.isdir(path) then
-        table.insert(mod_paths, {id=id, path=path})
-    end
-end
 
 local function getWorldModlist()
     -- ordered map of mod id -> {handled=bool, versions=map of version -> path}
@@ -452,10 +425,10 @@ local function getWorldModlist()
             path = tostring(path.value)
             -- skip vanilla "mods"
             if not path:startswith(INSTALLED_MODS_PATH) then goto continue end
-            local id = get_mod_id_and_version(path)
+            local id = scriptmanager.get_mod_id_and_version(path)
             if not id then goto continue end
             mods[id] = {handled=true}
-            add_mod_paths(mod_paths, id, path, '.')
+            scriptmanager.add_mod_paths(mod_paths, id, path, '.')
             ::continue::
         end
         local modlist = {}
@@ -485,10 +458,7 @@ function ModlistMenu:init()
             text_pen=COLOR_YELLOW,
             auto_width=true,
             on_activate=function()
-                local mods = ''
-                for _,mod in ipairs(getWorldModlist()) do
-                    mods = (mods == '' and mod) or (mods .. ', ' .. mod)
-                end
+                local mods = table.concat(getWorldModlist(), ', ')
                 dfhack.internal.setClipboardTextCp437(mods)
             end,
             enabled=function() return #modlist:getChoices() > 0 end,
